9becb67d58acf51b9144aa35237935c6e611bfc4
dljardim <dljardim@outlook.com>
2025-05-16 09:11:13 -0500

    Initial Commit

---
+++ DailyHabitList/DailyHabitListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHabitListApp.swift
+//  DailyHabitList
+//
+//  Created by Damian Jardim on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyHabitListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
